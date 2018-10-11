d5f14e300ee4984979aea49392514f8d278c2398
WINMAC12\2daw <gabrieldt007@hotmail.com>
2018-10-11 12:59:07 +0200

    subiendo trabajo realizado en clases con respecto a la actividad 2 de artículos científicos

--- EntornoCliente/actividad2/index.js
+++ EntornoCliente/actividad2/index.js
@@ -1,5 +1,54 @@
 const rl = require('readline-sync');
 const artCientifico = require('./objetos/articuloCientifico').articuloCientifico;
+var flag = true;
+var final = "";
+var opcion = 0;
+var titulo = "";
+var autores = [];
+var pagsArticulo = 0;
+var publicadoEl = 0;
+var menciones = 0;
+var listaArticulos = [];
+
+
+var articuloActual=titulo;
+
+console.log("*************************************************************************");
+console.log("** Bienvenido al sistema de publicación de descubrimientos científicos **");
+console.log("*************************************************************************");
+
+while (flag==true) {
+
+    console.log("Escriba:\n1) Si desea dar de alta un nuevo artículo científico\n2) Si desea dar de baja un artículo científico\n3) Si desea modificar los autores de un artículo científico\n4) Si desea modificar un artículo científico\n5) Si desea modificar las patentes científicas\n0) Si desea salir\n");
+    opcion = rl.questionInt();
+    if (opcion === 0) {
+        flag = false;
+    }else if(opcion === 1){
+        titulo = rl.question('Escriba el título del artículo científico: ');
+        let numautores = rl.question('Cuantos autores tiene éste artículo científico?: ');
+        for (let i = 0; i < numautores ; i++) {
+            autores[i] = rl.question('Escriba el nombre del autor ' + (i+1) + ': ');
+        }
+        var autoresPropios = new Array(autores);
+        pagsArticulo = rl.question('De cuantas páginas consta este artículo científico?: ');
+        publicadoEl = rl.question('Cuando fue publicado este artículo científico?: ');
+        menciones = rl.question('Cuantas menciones tiene este artículo científico?: ');
+
+        var articuloNuevo =  new artCientifico(titulo, autoresPropios, pagsArticulo, publicadoEl, menciones);
+        listaArticulos.push(articuloNuevo);
+
+        console.log(listaArticulos);
+
+    }else if (opcion === 2) {
+        
+    } else {
+        
+    }
+}
+
+
+/*
+const artCientifico = require('./objetos/articuloCientifico').articuloCientifico;
 var autores = ['hola', 'vale'];
 var articuloNuevo =  new artCientifico('La vida', autores, 20, 2, 3);
 console.log(articuloNuevo);
@@ -11,8 +60,16 @@ autores[2] = 'ramon';
 var autores2 = ['la vida', 'es', 'dificil'];
 articuloNuevo.cambiarAutores(autores2);
 console.log(articuloNuevo);
+*/
+/*
+var objetos = ['h', 'g', 'd'];
+var array = new Array (objetos);
 
-/*var art1 = new articuloCientifico(, )*/
+esto lo que hace es que te crea una copia del array que le estas pasando para que asi 
+cuando estes creando el objeto al modificar el array principal, no modificará el 
+almacenado en el objeto
+
+*/
 
 /*let newcar = new clase(matricula, marca, modelo, color, km);
 listadocoches.push(newcar); para crear un array con la información de newcar;
